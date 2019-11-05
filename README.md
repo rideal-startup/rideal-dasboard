d56ca38046ad7920ed2ea0a3c6f8544c7c2a4e1d
Guillem96 <guillem.orellana@gmail.com>
2019-11-05 19:56:22 +0100

    Add dashboard base code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rideal Dashboard
+
+## Licensing
+
+- Copyright 2019 Creative Tim (https://www.creative-tim.com?ref=bda-readme)
+- Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-angular/blob/master/LICENSE.md)
+
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,15 @@
+import { Routes } from "@angular/router";
+
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { MapComponent } from "../../pages/map/map.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "maps", component: MapComponent },
+  { path: "notifications", component: NotificationsComponent },
+  { path: "user", component: UserComponent },
+  { path: "tables", component: TablesComponent },
+];
