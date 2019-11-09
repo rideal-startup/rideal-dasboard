b48aaac82617acc7f76416c3caabfe06738438f9
ricard.cervera <ricard.cervera@ce.eurecat.org>
2019-11-09 15:14:15 +0100

    Update on fleet managment , form modified and find some error in item list from CDK(we are not able to drag items)

--- src/app/pages/fleet-managment/line-form/line-form.component.ts
+++ src/app/pages/fleet-managment/line-form/line-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-line-form',
@@ -8,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
 export class LineFormComponent implements OnInit {
 
   hexaColor:string = "#BC9166";
+  linePoints:mapPoint[] = [
+    new mapPoint(41.689426, 0.722586)
+    ,new mapPoint(11.689426, 0.422586)
+  ];
 
-  constructor() { }
+  isCircular:boolean = false;
+ movies = [
+  'Episode I - The Phantom Menace',
+  'Episode II - Attack of the Clones',
+  'Episode III - Revenge of the Sith',
+  'Episode IV - A New Hope',
+  'Episode V - The Empire Strikes Back',
+  'Episode VI - Return of the Jedi',
+  'Episode VII - The Force Awakens',
+  'Episode VIII - The Last Jedi'
+];
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+
+  public addStation(){
+    this.linePoints.push(new mapPoint(11.689426, 0.422586))
   }
 
+  public deletStation( index:number) {
+    this.linePoints.slice(index,index);
+  }
+
+  dropPoint(event: CdkDragDrop<mapPoint[]>) {
+    moveItemInArray(this.linePoints, event.previousIndex, event.currentIndex);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+
+}
+
+// tslint:disable-next-line: no-unused-expression
+class mapPoint {
+  public latitude: number;
+  public longitude: number;
+
+  constructor(latitude: number, longitude: number){
+    this.latitude = latitude;
+    this.longitude = longitude;
+    }
 }
