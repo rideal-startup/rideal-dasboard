3eb80d8f4bcc083c80cebf33725f69d85d49ab48
ricard.cervera <ricard.cervera@ce.eurecat.org>
2019-11-19 20:48:03 +0100

    -

--- src/app/pages/fleet-managment/fleet-managment.component.ts
+++ src/app/pages/fleet-managment/fleet-managment.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { Line } from 'src/app/domain/line';
 import { City } from 'src/app/domain/city';
 import { forkJoin } from 'rxjs';
+import { AuthenticationBasicService } from 'src/app/services/authentication-basic.service';
 
 
 @Component({
@@ -26,6 +27,7 @@ export class FleetManagmentComponent implements OnInit {
       postalCode: '',
     },
     routeType: 'CIRCULAR',
+    transportationMode: null,
     stops : [],
   } as Line;
 
@@ -34,12 +36,14 @@ export class FleetManagmentComponent implements OnInit {
   public selectedLine: Line = null;
   public loadingLines = true;
   public createMode = false;
+  public lineSelected = false;
 
   public message: {type?: string, msg?: string} = {};
 
   constructor(
     private lineService: LineService,
-    private cityService: CityService) { }
+    private cityService: CityService,
+    private auth: AuthenticationBasicService) { }
 
   ngOnInit() {
     const requests$ = [
@@ -59,19 +63,30 @@ export class FleetManagmentComponent implements OnInit {
 
   public setCreateLineMode() {
     this.selectedLine = new Line(this.emptyLine);
+    this.lineSelected = true;
     this.createMode = true;
   }
 
   public selectLine(line: Line) {
     this.createMode = false;
+    this.lineSelected = true;
     this.selectedLine = line;
   }
 
   public submit() {
+    if (!this.checkIfAllInputsComplete(this.selectedLine)) { return; }
+    this.selectedLine.company = this.auth.getCurrentUser();
+
     const request$ = this.createMode
       ? this.lineService.create(this.selectedLine)
       : this.lineService.update(this.selectedLine);
     request$.subscribe(res => {
+
+      if (this.createMode) {
+        this.lines.unshift(res);
+        this.lineSelected = false;
+      }
+
       this.message = {
         type: 'SUCCESS',
         msg: `Line ${this.createMode ? 'created' : 'updated'} successfully`
@@ -84,6 +99,67 @@ export class FleetManagmentComponent implements OnInit {
       };
     });
     setTimeout(() => this.message = null, 3000);
+  }
+
+  public delete() {
+    this.selectedLine.company = this.auth.getCurrentUser();
+
+    const request$ =
+    this.lineService.delete(this.selectedLine.id);
+    request$.subscribe(res => {
+      this.lineSelected = false;
+      this.lines.forEach((line, index) => {
+        if (line.id === this.selectedLine.id) {
+          this.lines.splice(index, 1);
+        }
+      });
+      this.message = {
+        type: 'SUCCESS',
+        msg: `Line ${this.createMode ? 'created' : 'updated'} successfully`
+      };
+    }, error => {
+      console.log(error);
+      this.message = {
+        type: 'ERROR',
+        msg: `Error ${this.createMode ? 'creating' : 'updating'} the line`
+      };
+    });
+    setTimeout(() => this.message = null, 3000);
+  }
+
+
+  private checkIfAllInputsComplete(line: Line): boolean {
+
+    let errrorFound = false;
+    if (!this.validateInput(line.available)) {return; }
+    if (!this.validateInput(line.length)) {return; }
+    if (!this.validateInput(line.onFreeDays)) {return; }
+    if (!this.validateInput(line.name)) {return; }
+    if (!this.validateInput(line.color)) {return; }
+    if (!this.validateInput(line.city)) {return; }
+
+    if (!this.validateInput(line.routeType)) {return; }
+    if (!this.validateInput(line.stops)) {return; } else {
+      line.stops.forEach( (stop) => {
+        if (!this.validateInput(stop.name)) { errrorFound = true; return; }
+        if (!this.validateInput(stop.location)) { errrorFound = true; return; } else {
+          if (!this.validateInput(stop.location.lat)) { errrorFound = true; return; }
+          if (!this.validateInput(stop.location.lng)) { errrorFound = true; return; }
+        }
+      });
+    }
+    if (errrorFound) {return; }
+    if (!this.validateInput(line.transportationMode)) {return; }
+
+    return true;
+  }
+
+  private validateInput(atribute: any) {
+    if (atribute === undefined || atribute === null) {
+      this.message = {type: 'ERROR', msg: `Not all the inputs are filled.`};
+      return false;
+    }
+    return true;
   }
 }
 
